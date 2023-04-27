de269c377ef453c4fcfd0ae97b44f192e94b4696
Максим Конев <111451752+Demustahl@users.noreply.github.com>
2023-04-27 15:13:45 +0400

    Update FirstTask.cs

--- Task 1/FirstTask.cs
+++ Task 1/FirstTask.cs
@@ -5,11 +5,18 @@ namespace Library
 {
     public class ZadanieOne
     {
-        public void One()
+        public static void One() /*need to add the static keyword so that the method
+         can be called without creating a class object*/
         {
             Console.Write("Введите число N: ");
-            int n = int.Parse(Console.ReadLine());
-            string result = string.Join(", ", Enumerable.Range(1, n)) + ".";
+            int n;
+            if ((!int.TryParse(Console.ReadLine(), out n) || (n < 0))) /* checking that it is possible to convert
+            a string entered by the user into a number and is it not less than 0 */
+            {
+                Console.WriteLine("Некорректный ввод.");
+                return;
+            }
+            string result = string.Join(", ", Enumerable.Range(1, n)) + "."; // adding a dot at the end of the line
             Console.WriteLine(result);
         }
     }
@@ -18,7 +25,12 @@ namespace Library
         public static void Two()
         {
             Console.Write("Введите нечетное число N: ");
-            int n = int.Parse(Console.ReadLine());
+            int n;
+            if (!int.TryParse(Console.ReadLine(), out n) || (n <= 0)) // checking for a number and is it not less than 0
+            {
+                Console.WriteLine("Некорректный ввод.");
+                return;
+            }
             if (n % 2 == 0) // If N is an even number, exit the function.
             {
                 Console.WriteLine("Вы ввели четное N.");
@@ -28,19 +40,19 @@ namespace Library
             {
                 for (int col = 1; col <= n; col++)
                 {
-                    if (row == n / 2 + 1 && col == n / 2 + 1) Console.Write("  ");
-                    else Console.Write("# ");
+                    if (row == n / 2 + 1 && col == n / 2 + 1) Console.Write("  "); /* we enter an additional 
+                    space so that the figure really looks like a square in the console */
+                    else Console.Write("# "); // here too
                 }
                 Console.WriteLine();
             }
         }
     }
-    class program
+    class program /* in the main function, we will call the methods of the classes */
     {
         static void Main()
         {
-            ZadanieOne zadanie1 = new ZadanieOne();
-            zadanie1.One();
+            ZadanieOne.One();
             ZadanieTwo.Two();
         }
     }
